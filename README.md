b32fad6a2fbf89fb6cf1b780683ee962e5bb1402
wangxianbing <wangxianbing@meituan.com>
2022-03-10 15:36:55 +0800

    增加tailwind和postcss;vite.config.ts中增加resolve;tsconfig.ts增加baseUrl和paths

--- README.md
+++ README.md
@@ -3,17 +3,39 @@
 ## 1、安装nodejs和npm
 ## 2、安装yarn
 ```
-$>npm install -g yarn
+npm install -g yarn
 ```
 ## 3、生成vite项目
 ```
-$>yarn create vite
+yarn create vite
 ```
 ## 4、安装node modules依赖包
 ```
-$>yarn
+yarn
 ```
 ## 5、运行并查看效果
 ```
-$>yarn dev
+yarn dev
+```
+## 6、安装tailwind 和 postcss
+```
+yarn add tailwindcss@latest postcss@latest autoprefixer@latest
+```
+
+## 7、生成tailwind和postcss配置文件
+```
+npx tailwindcss init -p
+```
+
+## 8、修改vite.config.ts
+```
+
+```
+
+## 9、
+```
+```
+
+## 10、
+```
 ```
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+import { resolve } from 'path' //必须要引入resolve 
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+
+  //必须要引入resolve 
+  resolve: {
+    alias: [
+      {
+        find: "@",
+        replacement: resolve(__dirname, 'src')
+      },
+      {
+        find: "@assets",
+        replacement: resolve(__dirname, 'src/assets')
+      }
+    ]
+  }
 })
